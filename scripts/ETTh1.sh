8cb627d5984d5b47abd49040ed0e9f61b0b08605
MDK-L <brumaire18th@gmail.com>
2024-07-05 20:49:01 +0800

    update

---
+++ scripts/ETTh1.sh
@@ -0,0 +1,17 @@
+for len in 96 192 336 720
+do
+  python -u Forecasting/TSLANet_Forecasting_1Mamba_v0_2_silu_filter_freq.py \
+  --root_path datasets/ETT-small \
+  --pred_len $len \
+  --data ETTh1 \
+  --data_path ETTh1.csv \
+  --seq_len 512 \
+  --emb_dim 64 \
+  --depth 1 \
+  --batch_size 512 \
+  --dropout 0.5 \
+  --patch_size 32 \
+  --train_epochs 20 \
+  --pretrain_epochs 10 \
+  --ASB False
+done
