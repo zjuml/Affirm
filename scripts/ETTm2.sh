945c1e6f0340b60b877978014cd366c62718e85d
Hu <hhj12345678hhj@gmail.net>
2024-07-07 17:27:05 +0800

    add scripts

---
+++ scripts/ETTm2.sh
@@ -0,0 +1,16 @@
+for len in 96 192 336 720
+do
+  python -u TSLANet_Forecasting_1Mamba_v0_2_silu_filter_freq.py \
+  --root_path /mnt/sdb/hhj/TSLANet/all_datasets/ETT-small/ \
+  --pred_len $len \
+  --data ETTm1 \
+  --data_path ETTm1.csv \
+  --seq_len 512 \
+  --emb_dim 128 \
+  --depth 1 \
+  --batch_size 512 \
+  --dropout 0.5 \
+  --patch_size 64 \
+  --train_epochs 50 \
+  --pretrain_epochs 10 > mamba_${data_path}_train_epochs_${train_epochs}'_'len_$len.log
+done
